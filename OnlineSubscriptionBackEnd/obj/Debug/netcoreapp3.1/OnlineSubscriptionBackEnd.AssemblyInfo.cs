6e8c2b81a614b42474721c784954a469b11367e4
Himal Giri <guru@insoftnepal.com>
2024-11-08 12:55:27 +0545

    as

--- OnlineSubscriptionBackEnd/obj/Debug/netcoreapp3.1/OnlineSubscriptionBackEnd.AssemblyInfo.cs
+++ OnlineSubscriptionBackEnd/obj/Debug/netcoreapp3.1/OnlineSubscriptionBackEnd.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineSubscriptionBackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+635685374d82a2c9744731761af7a34111a27c0c")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0182961fb223e204550ecdc8610ba89b294c148b")]
->>>>>>> 069cf05ff550b6d70456ba1c1c4b8dd1da1111d6
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce5f726d37505832ebff8ee415f910c6c60cbd37")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineSubscriptionBackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineSubscriptionBackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
