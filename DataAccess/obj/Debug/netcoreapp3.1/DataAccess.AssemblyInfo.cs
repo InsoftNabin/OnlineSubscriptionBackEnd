721404259eef78a4e1e0297672537f326d62161a
Himal Giri <guru@insoftnepal.com>
2024-11-10 12:16:12 +0545

    a

--- DataAccess/obj/Debug/netcoreapp3.1/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/netcoreapp3.1/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e2714a6fa5ba52b2eff50ee8d2b5cb66539f1d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e066476b95cbfe4b5a098180b2d3ad4d5eb3c648")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
